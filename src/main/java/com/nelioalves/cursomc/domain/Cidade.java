28681bc66290382169cc7d8829867d18d69bfb49
Vagner Almeida <valmeida1964@hotmail.com>
2023-11-10 18:59:03 -0300

    Endpoint /clientes/{id} disponivel

--- src/main/java/com/nelioalves/cursomc/domain/Cidade.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cidade.java
@@ -3,6 +3,9 @@ package com.nelioalves.cursomc.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,6 +22,7 @@ public class Cidade implements Serializable{
 	private Integer id;
 	private String nome;
 	
+	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name="estado_id")
 	private Estado estado;
--- src/main/java/com/nelioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -7,6 +7,8 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.nelioalves.cursomc.domain.enuns.TipoCliente;
 
 import jakarta.persistence.CollectionTable;
@@ -29,6 +31,7 @@ public class Cliente  implements Serializable{
 	private String 	cpfOuCnpj;
 	private Integer tipo;
 	
+	@JsonManagedReference
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
